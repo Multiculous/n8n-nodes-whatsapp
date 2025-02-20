0f324301857073eefbac0928720a8e19504f3a50
devlikepro <waha@devlike.pro>
2025-02-20 16:43:11 +0700

    Add 2025.2.5

--- nodes/WAHA/WAHA.node.ts
+++ nodes/WAHA/WAHA.node.ts
@@ -2,16 +2,18 @@ import type { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow'
 import { VersionedNodeType } from 'n8n-workflow';
 import { WAHAv202409 } from './v202409/WAHAv202409';
 import {BASE_DESCRIPTION} from "./base/node";
+import {WAHAv202502} from "./v202502/WAHAv202502";
 
 export class WAHA extends VersionedNodeType {
 	constructor() {
 		const baseDescription: INodeTypeBaseDescription = {
 			...BASE_DESCRIPTION,
-			defaultVersion: 202409,
+			defaultVersion: 202502,
 		};
 
 		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
 			202409: new WAHAv202409(),
+			202502: new WAHAv202502(),
 		};
 
 		super(nodeVersions, baseDescription);
--- nodes/WAHA/WAHATrigger.node.ts
+++ nodes/WAHA/WAHATrigger.node.ts
@@ -2,16 +2,18 @@ import type { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow'
 import { VersionedNodeType } from 'n8n-workflow';
 import {BASE_TRIGGER_DESCRIPTION} from "./base/trigger";
 import {WAHATriggerV202409} from "./v202409/WAHATriggerV202409";
+import {WAHATriggerV202502} from "./v202502/WAHATriggerV202502";
 
 export class WAHATrigger extends VersionedNodeType {
 	constructor() {
 		const baseDescription: INodeTypeBaseDescription = {
 			...BASE_TRIGGER_DESCRIPTION,
-			defaultVersion: 202409,
+			defaultVersion: 202502,
 		};
 
 		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
 			202409: new WAHATriggerV202409(),
+			202502: new WAHATriggerV202502(),
 		};
 
 		super(nodeVersions, baseDescription);
